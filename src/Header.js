b716b2f59fa32cc65ae114096f4ade09f06751bb
generic-user-1234 <vihaan.sood0@gmail.com>
2024-06-16 21:46:27 +0100

    Second commit

--- src/Header.js
+++ src/Header.js
@@ -1,15 +1,19 @@
 import logo from './freshfeed.png';
 import reload_button from './Reload button freshfeed.png'
 import React from 'react';
+import "./Header.css" 
 
 
 const Header = () =>  {
   return (
       <>
-      <img src={logo} alt="Logo" />
+      <img className = "logo" src={logo} alt="Logo" />
+      <div className="vl">
+      
       <a href='/'>
-      <img src={reload_button} alt="Logo" />
+      <img className = "reload" src={reload_button} alt="Logo" />
       </a>
+      </div>
       </>
   );
 }
